fa44c43ee640385ed1c15a0cb287d4635157bb2d
ssrlive <30760636+ssrlive@users.noreply.github.com>
2025-11-28 17:49:08 +0800

    Improve console.log output formatting
    - Add detailed array printing with element values
    - Add object property printing with key-value pairs
    - Improve function printing with parameter names and code type
    - Add class name printing for ClassDefinition
    - Enhance Property descriptor printing with available attributes

--- src/js_console.rs
+++ src/js_console.rs
@@ -24,13 +24,81 @@ pub fn handle_console_method(method: &str, args: &[Expr], env: &JSObjectDataPtr)
                     }
                     Value::Boolean(b) => print!("{}", b),
                     Value::Undefined => print!("undefined"),
-                    Value::Object(_) => print!("[object Object]"),
-                    Value::Function(name) => print!("[Function: {}]", name),
-                    Value::Closure(_, _, _) => print!("[Function]"),
-                    Value::ClassDefinition(_) => print!("[Class]"),
+                    Value::Object(obj) => {
+                        if crate::js_array::is_array(&obj) {
+                            // Print array contents
+                            let len = crate::js_array::get_array_length(&obj).unwrap_or(0);
+                            print!("[");
+                            for i in 0..len {
+                                if i > 0 {
+                                    print!(",");
+                                }
+                                if let Some(val_rc) = crate::core::obj_get_value(&obj, i.to_string())? {
+                                    match &*val_rc.borrow() {
+                                        Value::Number(n) => print!("{}", n),
+                                        Value::String(s) => print!("\"{}\"", String::from_utf16_lossy(s)),
+                                        Value::Boolean(b) => print!("{}", b),
+                                        Value::Undefined => print!("undefined"),
+                                        _ => print!("[object Object]"),
+                                    }
+                                }
+                            }
+                            print!("]");
+                        } else {
+                            // Print object properties
+                            print!("{{");
+                            let mut first = true;
+                            for (key, val_rc) in obj.borrow().properties.iter() {
+                                if !first {
+                                    print!(", ");
+                                }
+                                first = false;
+                                print!("{}: ", key);
+                                match &*val_rc.borrow() {
+                                    Value::Number(n) => print!("{}", n),
+                                    Value::String(s) => print!("\"{}\"", String::from_utf16_lossy(s)),
+                                    Value::Boolean(b) => print!("{}", b),
+                                    Value::Undefined => print!("undefined"),
+                                    Value::Object(inner_obj) => {
+                                        if crate::js_array::is_array(inner_obj) {
+                                            print!("[Array]");
+                                        } else {
+                                            print!("[object Object]");
+                                        }
+                                    }
+                                    _ => print!("[object Object]"),
+                                }
+                            }
+                            print!("}}");
+                        }
+                    }
+                    Value::Function(name) => print!("function {}() {{ [native code] }}", name),
+                    Value::Closure(params, _, _) => {
+                        print!("function(");
+                        for (i, param) in params.iter().enumerate() {
+                            if i > 0 {
+                                print!(", ");
+                            }
+                            print!("{}", param);
+                        }
+                        print!(") {{ [closure code] }}");
+                    }
+                    Value::ClassDefinition(ref class_def) => print!("class {}", class_def.name),
                     Value::Getter(_, _) => print!("[Getter]"),
                     Value::Setter(_, _, _) => print!("[Setter]"),
-                    Value::Property { .. } => print!("[Property]"),
+                    Value::Property { value, getter, setter } => {
+                        print!("[Property");
+                        if value.is_some() {
+                            print!(" value");
+                        }
+                        if getter.is_some() {
+                            print!(" getter");
+                        }
+                        if setter.is_some() {
+                            print!(" setter");
+                        }
+                        print!("]");
+                    }
                 }
             }
             println!();
